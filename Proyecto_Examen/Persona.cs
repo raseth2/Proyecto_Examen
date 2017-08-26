efcc798b6410d511026242486ffc35b9f9277392
jeos <jotaele89@gmail.com>
2017-08-26 11:28:48 -0500

    tabla Persona

---
+++ Proyecto_Examen/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Examen
+{
+    public class Persona
+    {
+        public int DNI { set; get; }
+        public int Codigo { set;get }
+        public string Nombres { set; get; }
+        public String ApellidoPaterno { set; get; }
+        public string ApellidoMaterno { set; get; }
+        public int telefono { set; get; }
+
+    }
+}
