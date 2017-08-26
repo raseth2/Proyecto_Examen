cfae04ed78c4a7bba52f4df27efe5c35951caa3d
AEPIS07 <AEPIS07>
2017-08-26 10:39:44 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_Examen/RespuestaExamenAlumno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Examen
+{
+    class RespuestaExamenAlumno
+    {
+        public int CodigoExamenAlumno { get; private set; }
+        public string RespuestaExamenAl { get; private set; }
+        public bool VerificarRespuesta { get; private set; }
+  
+        public virtual ExamenAlumno examenAlumno { get; set; }
+
+
+        public void ValidarRespuesta()
+        {
+            VerificarRespuesta = true;
+
+        }
+        public static RespuestaExamenAlumno RegistrarRespuesta(int ainCodigoExamenAlumno, string astRespuesta, bool aboVerificarRespuesta)
+        {
+            return new RespuestaExamenAlumno()
+            {
+                CodigoExamenAlumno = ainCodigoExamenAlumno,
+                RespuestaExamenAl = astRespuesta,
+                VerificarRespuesta = aboVerificarRespuesta
+            };
+        }
+    }
+}
