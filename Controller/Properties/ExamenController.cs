53de1f71807d00018040b87f6dbf6539f1e569a9
Samuel <raseth2.cek22@gmail.com>
2017-08-26 12:18:40 -0500

    Agregando Controller

---
+++ Controller/Properties/ExamenController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Controller.Properties
+{
+    public class ExamenController : ApiController
+    {
+        // GET: api/Examen
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Examen/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Examen
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Examen/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Examen/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
