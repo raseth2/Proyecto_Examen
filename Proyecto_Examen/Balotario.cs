a7be02735144c7a3647e85bec4359da23afd565a
jeos <jotaele89@gmail.com>
2017-08-26 12:21:57 -0500

    tabla balotario

---
+++ Proyecto_Examen/Balotario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Examen
+{
+    public class Balotario
+    {
+        public int CodigoBalotario { set; get; }
+        public string NombreBalotario { set; get; }
+        public DateTime FechaCreacion { set; get; }
+        public string Indicaciones { set; get; }
+        public string Estado { set; get; }
+    }
+}
