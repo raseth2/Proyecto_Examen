8910fab24d1424d9fb6d86eb171f3ee869f51f05
Ban Vilca Mido <banvilca251@hotmail.es>
2017-09-18 10:04:08 -0500

    añadir metodos

--- Proyecto_Examen/Balotario.cs
+++ Proyecto_Examen/Balotario.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 namespace Proyecto_Examen
 {
     public class Balotario
-    {
+        {
         public int CodigoBalotario { set; get; }
         public string NombreBalotario { set; get; }
         public DateTime FechaCreacion { set; get; }
         public string Indicaciones { set; get; }
         public string Estado { set; get; }
-    }
+
+         
+         }
 }
